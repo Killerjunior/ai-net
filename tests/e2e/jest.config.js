99fd9e46cfdb1f3a94153e57b23be290ea4113ff
Umeokonkwo Samuel <umeokonkwosamuel@gmail.com>
2026-07-20 20:34:33 +0100

    fix(e2e): update jest config transformer and root package dependencies for CI (#121)

--- tests/e2e/jest.config.js
+++ tests/e2e/jest.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: path.resolve(__dirname, '../../'),
   testMatch: ['<rootDir>/tests/e2e/**/*.test.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }]
   },
